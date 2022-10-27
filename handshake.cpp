c7ec9f6c0fd5bc19f01fcf72afced75581e8caaa
Yassin Dolliazal <ydolliazal@outlook.com>
2022-10-27 17:41:23 -0500

    Updated handshake.cpp

--- handshake.cpp
+++ handshake.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 // include additional necessary headers
+#include <string> // string
+#include <iostream> // cout, endl
+#include <thread> // thread, join
+#include <stdlib.h> // atoi
+#include <vector> // vector
 
-void query(/*add necessary parameters*/) {
+using namespace std;
+
+void query(int n) {
     // Should print: the print number (starting from 0), "SYN", and the three dots "..."
+    cout << "[" << n <<"] SYN ... ";
 }
 
-void response(/*add necessary parameters*/) {
+void response() {
     // Should print "ACK"
+    cout << "ACK" << endl;
 }
 
 int main(int argc, char** argv) {
@@ -23,6 +32,18 @@ int main(int argc, char** argv) {
      * 4. Provide the threads with necessary args
      * 5. Update the "query" and "response" functions to synchronize the output
     */
-   
+
+    int count = atoi(argv[1]);
+    vector<thread> t_vec;
+
+    for (int i = 0; i < count; i++) {
+        t_vec.push_back(thread(query, i));
+        t_vec.push_back(thread(response));
+    }
+
+    for (int i=0; i < t_vec.size(); i++) {
+        t_vec.at(i).join();
+    }
+
     return 0;
 }
